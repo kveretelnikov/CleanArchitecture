e507f5ef195a19a2d45edd8935baf62e35fa6f89
Konstantin <konstantin.veretelnikov@saritasa.com>
2018-08-05 20:40:07 +0700

    Add save state in interactor

--- app/src/main/java/com/leventime/qualificationproject/features/login/presentation/LoginPresenterImpl.java
+++ app/src/main/java/com/leventime/qualificationproject/features/login/presentation/LoginPresenterImpl.java
@@ -122,5 +122,6 @@ public class LoginPresenterImpl extends BasePresenterImpl<LoginView> implements 
         clearDisposables();
         mState.setOwner(this);
         mState.onEnter(getView());
+        mInteractor.setState(mState);
     }
 }
